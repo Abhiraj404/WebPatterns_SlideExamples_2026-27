cf4f9ceeac4b1f1672d2005e355aef4e6b83ad41
michelle <michelle.graham@dkit.ie>
2026-09-15 00:31:46 +0100

    feat: Example code for database interaction in Java

---
+++ src/main/java/webpatterns/webpatterns_slideexamples_202627/WebPatternsSlideExamples202627Application.java
@@ -0,0 +1,13 @@
+package webpatterns.webpatterns_slideexamples_202627;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPatternsSlideExamples202627Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebPatternsSlideExamples202627Application.class, args);
+    }
+
+}
---
+++ src/test/java/webpatterns/webpatterns_slideexamples_202627/WebPatternsSlideExamples202627ApplicationTests.java
@@ -0,0 +1,13 @@
+package webpatterns.webpatterns_slideexamples_202627;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPatternsSlideExamples202627ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
